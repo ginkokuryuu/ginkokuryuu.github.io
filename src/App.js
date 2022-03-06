3d53ed7bae93f9b9272ffbc5f64ad90385eb783f
Habib Yahya <nmh.habib.y@gmail.com>
2022-03-06 23:48:44 +0700

    [Add] Data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import Profile from './components/Profile'
+import IibAndIfa from './routes/IibAndIfa';
 import './App.css'
 import ProjectShowcase from './components/ProjectShowcase';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-        <Profile/>
-        <ProjectShowcase/>
-    </div>
+    <Router>
+      <div className="App">
+          <Routes>
+            <Route path='/' exact element={
+              <div>
+                <Profile/>
+                <ProjectShowcase/>
+              </div>
+            }/>
+            <Route path="IibAndIfa" element={<IibAndIfa/>}/>
+          </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/routes/IibAndIfa.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function IibAndIfa() {
+  return (
+    <div style={{color:"white"}}>IibAndIfa</div>
+  )
+}
